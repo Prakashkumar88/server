4770cd753e7b5b2a82f10a15cc9972ba18d93148
Prakashkumar88 <prakashkumar88@gmail.com>
2025-12-17 13:49:51 +0530

    refactor: Update PrismaClient import and enhance logging configuration

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,26 @@
 import "../env.js";
-import { PrismaClient } from "@prisma/client";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis;
+// console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
+// Create a pg pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Create Prisma adapter
+const adapter = new PrismaPg(pool);
+
+// Lazy singleton
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["error"],
+    adapter,
+    log: ["error", "warn"],
   });
 
 if (process.env.NODE_ENV !== "production") {
