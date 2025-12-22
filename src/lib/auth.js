3dd57d3e863423c416f2d89d75d9513b387f114b
Prakashkumar88 <prakashkumar88@gmail.com>
2025-12-22 12:58:24 +0530

    refactor: Set trust proxy in index.js and update trusted origins in auth.js

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,19 +1,26 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { deviceAuthorization } from "better-auth/plugins"; 
+import { deviceAuthorization } from "better-auth/plugins";
 import prisma from "./db.js";
-
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  cookies: {
+    secure: true,       // REQUIRED (https)
+    sameSite: "none",   // REQUIRED (cross-site OAuth)
+    domain: ".railway.app",
+  },
   basePath: "/api/auth",
-  trustedOrigins: ["https://client-aj2q.vercel.app"],
+  trustedOrigins: [
+    "https://client-aj2q.vercel.app",
+    "https://server-production-a027.up.railway.app",
+  ],
   plugins: [
-    deviceAuthorization({ 
-      verificationUri: "/device", 
-    }), 
+    deviceAuthorization({
+      verificationUri: "/device",
+    }),
   ],
   socialProviders: {
     github: {
