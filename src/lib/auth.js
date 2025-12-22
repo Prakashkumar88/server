c460f2abb401f59c00f5700be37260d33fbaee94
Prakashkumar88 <prakashkumar88@gmail.com>
2025-12-22 23:23:15 +0530

    cros-origin cookie setup

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,19 +1,22 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { deviceAuthorization } from "better-auth/plugins"; 
+import { deviceAuthorization } from "better-auth/plugins";
 import prisma from "./db.js";
-
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
   basePath: "/api/auth",
+  cookies: {
+    sameSite: "none", // REQUIRED for cross-site OAuth
+    secure: true, // REQUIRED for HTTPS
+  },
   trustedOrigins: ["https://lapras-cli.vercel.app"],
   plugins: [
-    deviceAuthorization({ 
-      verificationUri: "/device", 
-    }), 
+    deviceAuthorization({
+      verificationUri: "/device",
+    }),
   ],
   socialProviders: {
     github: {
